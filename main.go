37bec6a21715f2899312fc68b9c225053f9bf274
crunchy89 <rocker.hunt@gmail.com>
2024-01-27 12:54:50 +0800

    adding another deploy

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func main() {
 	e.Any("/socket", echo.WrapHandler(handler.ApiChannel(socketWsHandler)))
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Server Health Up and Update")
+		return c.String(http.StatusOK, "Server Health Up")
 	})
 
 	logger.Fatal(e.Start(os.Getenv("PORT")))
